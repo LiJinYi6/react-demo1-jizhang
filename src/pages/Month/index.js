3e7fbc9f31d1950d792509f44900e8254c7c4fbe
LJY <3203345259@qq.com>
2025-06-17 09:50:08 +0800

    topCard统计区域全部完成包括计算

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -5,11 +5,11 @@ import { DownOutline, UpOutline } from 'antd-mobile-icons';
 import { asyncSetBillList } from "@/store/modules/billStore";
 import './index.scss';
 import dayjs from "dayjs";
-import _ from 'lodash'
+import _, { set } from 'lodash'
 import { useDispatch, useSelector } from "react-redux";
 function Month() {
   const [isShowTime, setIsShowTime] = useState(false);
-  const [currentMonth, setCurrentMonth] = useState(dayjs(new Date()));
+  const [currentMonth, setCurrentMonth] = useState(dayjs(new Date()).format('YYYY-MM'));
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(asyncSetBillList());
@@ -18,28 +18,39 @@ function Month() {
   const monthGroup = useMemo(() => {
     return _.groupBy(billList,(item) => dayjs(item.date).format('YYYY-MM'))
   },[billList])
-  console.log(monthGroup)
+  const currentBillList = useMemo(() => {
+    return monthGroup[currentMonth] || [];
+  }, [currentMonth,monthGroup])
+  const moneyResult = useMemo(() => {
+    console.log('计算当前月的收支', currentBillList)
+    const incomeTotal = currentBillList.filter(item => item.type === 'income').reduce((a,c) => {return a+c.money},0)
+    const payTotal = currentBillList.filter(item => item.type === 'pay').reduce((a,c) => {return a+c.money},0)
+    const total = incomeTotal + payTotal
+    console.log('计算结果', incomeTotal, payTotal, total   )
+    return { incomeTotal, payTotal, total }
+  },[currentBillList])
+
   return (
     <div className = "month">
       <NavBar backIcon={false}>月度收支</NavBar>
       <div className='top-card'>
         <div className='time-ctl'>
-            <span>{dayjs(currentMonth).format('YYYY | M月账单')}</span>
+            <span>{`${currentMonth} 账单 `}</span>
             {!isShowTime && <span onClick={() => setIsShowTime(!isShowTime)}><DownOutline /></span>}
             {isShowTime && <span onClick={() => setIsShowTime(!isShowTime)}><UpOutline /></span>}
         </div>
         <div className="total">
             <div className='item'>
                 <span className='label'>收入</span>
-                <span className='value'>¥ 10000</span>
+                <span className='value'>{moneyResult.incomeTotal}</span>
             </div>
             <div className='item'>
                 <span className='label'>支出</span>
-                <span className='value'>¥ 5000</span>
+                <span className='value'>{moneyResult.payTotal}</span>
             </div>
             <div className='item'>
                 <span className='label'>结余</span>
-                <span className='value'>¥ 5000</span>
+                <span className='value'>{moneyResult.total} </span>
             </div>
         </div>
       </div>
@@ -52,8 +63,7 @@ function Month() {
           }}
           precision='month'
           onConfirm={val => {
-            setCurrentMonth(val)
-            console.log(currentMonth)
+            setCurrentMonth(dayjs(val).format('YYYY-MM'))
           }}
         />
     </div>
